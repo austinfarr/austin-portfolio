a56ee3b9ec1f5340c23d62d4cb4e5b5af130633e
Austin Farr <austin@farrnet.com>
2025-08-05 07:43:01 -0500

    chore: remove illustration in about section

--- components/About.jsx
+++ components/About.jsx
@@ -1,13 +1,10 @@
 import {
   Briefcase,
-  Calendar,
   GraduationCap,
   HomeIcon,
   MailIcon,
-  PhoneCall,
   User2,
 } from "lucide-react";
-import { DevImg } from "./DevImg";
 import Image from "next/image";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "./ui/tabs";
 
@@ -99,18 +96,11 @@ const About = () => {
         <h2 className="section-title mb-8 xl:mb-16 text-center mx-auto">
           About Me
         </h2>
-        <div className="flex flex-col xl:flex-row">
-          {/* image */}
-          <div className="hidden xl:flex flex-1 relative">
-            <DevImg
-              containerStyles="bg-about_shape_light dark:bg-about_shape_dark w-[505px] h-[505px] bg-no-repeat relative"
-              imgSrc="/about/AboutUs.svg"
-            />
-          </div>
+        <div className="flex justify-center">
           {/* tabs */}
-          <div className="flex-1">
+          <div className="w-full max-w-3xl">
             <Tabs defaultValue="work">
-              <TabsList className="w-full grid grid-cols-2 xl:max-w-[520px] border dark:border-none">
+              <TabsList className="w-full grid grid-cols-2 border dark:border-none mx-auto">
                 <TabsTrigger className="w-auto" value="work">
                   My Work
                 </TabsTrigger>
@@ -122,22 +112,22 @@ const About = () => {
               <div className="text-lg mt-12 xl:mt-8">
                 {/* Personal Info Content */}
                 <TabsContent value="work">
-                  <div className="text-center xl:text-left">
+                  <div className="text-center">
                     <h3 className="h3 mb-4">I love bringing ideas to life</h3>
-                    <p className="subtitle max-w-xl mx-auto xl:mx-0 text-left ">
+                    <p className="subtitle max-w-xl mx-auto">
                       As a full-stack Web Developer, I specialize in crafting
                       intuitive websites with cutting-edge technology,
                       delivering dynamic and engaging user experiences{" "}
                     </p>
                   </div>
 
-                  <div>
-                    <h4 className="text-xl font-semibold mb-2 xl:text-left text-center">
+                  <div className="mt-8">
+                    <h4 className="text-xl font-semibold mb-2 text-center">
                       Some of My Tools
                     </h4>
                     <div className="border-b border-border mb-4"></div>
-                    <div className="flex xl:justify-start justify-center">
-                      <div className="flex flex-wrap gap-x-8 gap-y-4 justify-center xl:justify-start max-w-[500px] xl:max-w-[600px]">
+                    <div className="flex justify-center">
+                      <div className="flex flex-wrap gap-x-8 gap-y-4 justify-center max-w-[600px]">
                         {getData(skillData, "tools").data.map((item, index) => {
                           const { imgPath } = item;
                           return (
@@ -173,15 +163,13 @@ const About = () => {
                 {/* Qualifications Content */}
                 <TabsContent value="experience">
                   <div>
-                    <h3 className="h3 mb-8 text-center xl:text-left">
-                      My Journey
-                    </h3>
+                    <h3 className="h3 mb-8 text-center">My Journey</h3>
                     {/* icons */}
-                    <div className="grid xl:grid-cols-2 xl:gap-4 gap-2 xl:mb-12 mb-6">
+                    <div className="grid sm:grid-cols-2 gap-4 mb-12 max-w-lg mx-auto">
                       {infoData.map((item, index) => {
                         return (
                           <div
-                            className="flex items-center gap-x-4 xl:mx-0"
+                            className="flex items-center gap-x-4"
                             key={index}
                           >
                             <div className="text-primary">{item.icon}</div>
@@ -191,10 +179,10 @@ const About = () => {
                       })}
                     </div>
                     {/* Experience and Education Wrapper */}
-                    <div className="grid md:grid-cols-2 gap-y-8">
+                    <div className="grid md:grid-cols-2 gap-8 max-w-2xl mx-auto">
                       {/* Experience */}
                       <div className="flex flex-col gap-y-6">
-                        <div className="flex gap-x-4 items-center text-[22px] text-primary dark:text-white">
+                        <div className="flex gap-x-4 items-center text-[22px] text-primary dark:text-white justify-center md:justify-start">
                           <Briefcase />
                           <h4 className="capitalize font-medium ">
                             {getData(qualificationsData, "experience").title}
@@ -232,7 +220,7 @@ const About = () => {
                       </div>
                       {/* Education */}
                       <div className="flex flex-col gap-y-6">
-                        <div className="flex gap-x-4 items-center text-[22px] text-primary dark:text-white">
+                        <div className="flex gap-x-4 items-center text-[22px] text-primary dark:text-white justify-center md:justify-start">
                           <GraduationCap size={28} />
                           <h4 className="capitalize font-medium ">
                             {getData(qualificationsData, "education").title}
