db2d6b7aa07aea86248cb6c25d7887dcf714c30b
Austin Farr <austin@farrnet.com>
2025-08-05 22:50:06 -0500

    feat: remove services component

--- app/page.js
+++ app/page.js
@@ -2,20 +2,16 @@ import About from "@/components/About";
 import Blogs from "@/components/Blogs";
 import { CallToAction } from "@/components/CallToAction";
 import Hero from "@/components/Hero";
-import Reviews from "@/components/Reviews";
-import Services from "@/components/Services";
 import Work from "@/components/Work";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <main>
       <Hero />
       <About />
-      <Services />
       <Work />
       <Blogs />
-      {/* <Reviews /> */}
+
       <CallToAction />
     </main>
   );
